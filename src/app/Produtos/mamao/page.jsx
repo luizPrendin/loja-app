a9801b9f45b830269b96aff4b9039909bda8fc95
luizPrendin <luizprendinfelipe@gmail.com>
2023-10-16 15:11:16 -0300

    iniciando Next e realizando estilização c tailwind,Realizando passagem de dados via componentes.

---
+++ src/app/Produtos/mamao/page.jsx
@@ -0,0 +1,8 @@
+import Produtos from "@/components/Produtos/Produtos";
+import React from "react";
+
+export default function Mamao(){
+    return(
+        <Produtos prod={"Mamão"}/>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className="w-52 m-auto bg-red-500 text-center font-bold text-3xl rounded-lg"> Home</h1>
+    </div>
+  )
+}
