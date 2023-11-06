10913be2a929fe0c6f7d138a1925f9a835958d6d
luizPrendin <luizprendinfelipe@gmail.com>
2023-11-06 11:52:31 -0300

    Validação de login na API de usuarios

--- src/app/api/base/base-users/route.jsx
+++ src/app/api/base/base-users/route.jsx
@@ -15,3 +15,28 @@ export async function GET(){
   //Retornando os dados do arquivo
   return NextResponse.json(listaUsuarios.usuarios);
 }
+export async function POST(request,response) {
+  //Realizando a leitura do arquivo db.json desde a raiz do projeto
+  const file = await fs.readFile(
+    process.cwd() + "/src/app/api/base/db.json",
+    "utf-8"
+  );
+
+  //recebendos os dados do formulario pelo request
+    const userLogin = await request.json();
+
+
+  const listaUsuarios = await JSON.parse(file);
+
+    //Logica de validação
+    const userValid = listaUsuarios.usuarios.find((user)=> user.email == userLogin.email && user.senha == userLogin.senha)
+
+    //verificando se o usuario foi validado
+    if(!userValid){
+        //Retornando os status da validação
+        return NextResponse.json({ "status": false});
+    }
+
+  //Retornando os dados do arquivo
+  return NextResponse.json({"status":true},userValid);
+}
