ed7579f32d5575a4069985e82535fa46f2e18a9b
luizPrendin <luizprendinfelipe@gmail.com>
2023-11-06 12:11:58 -0300

    Criação da view login, apenas Front sem lógica.

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -3,16 +3,16 @@ import Link from 'next/link'
 
 export default function Cabecalho() {
   return (
-    <header className='cabecalho'>
-          <nav>
-            <Link href="/" >HOME</Link>
-            <Link href="/consumo-user/usuarios-view">USUARIOS</Link>
-            <Link href="/consumo/frutas-view" >FRUTAS</Link>
-            <Link href="/Produtos/mamao" >MAMÃO</Link>
-            <Link href="/Produtos/melao" >MELÃO</Link>
-            <Link href="/Produtos/manga" >MANGA</Link>
-          </nav>
-        </header>
-
-  )
+    <header className="cabecalho">
+      <nav>
+        <Link href="/">HOME</Link>
+        <Link href="/login">Login</Link>
+        <Link href="/consumo-user/usuarios-view">USUARIOS</Link>
+        <Link href="/consumo/frutas-view">FRUTAS</Link>
+        <Link href="/Produtos/mamao">MAMÃO</Link>
+        <Link href="/Produtos/melao">MELÃO</Link>
+        <Link href="/Produtos/manga">MANGA</Link>
+      </nav>
+    </header>
+  );
 }
